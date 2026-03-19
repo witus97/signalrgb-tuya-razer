9a5bac07c9edbb3b991242bc39c4cec35b69c379
witus97 <hellground97@gmail.com>
2026-03-19 03:37:36 +0100

    Update DeviceList.test.js

--- Data/DeviceList.test.js
+++ Data/DeviceList.test.js
@@ -30,7 +30,7 @@ export default {
     // "dkg8z2bvjz9imahs": { leds: [1,2,3] },
     // "hirevagkjjq3abnn": { leds: [1,2,3] },
     // "mn60o1u7xv8qh8f1": { leds: [1,2,3] },
-    "stmkcsykq3kheboa": { leds: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], name: "Battletron Light Bar" },
+    "jwiudkdzdd7agu2o": { leds: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], name: "Battletron Light Bar" },
     "yrg6a649fkozp1hw": { leds: [1], name: "Battletron Ball Light" },
     // "m2jqc9ocwnxdsgs8": { leds: [1,2,3] },
     // "8cml7rj948sagivg": { leds: [1,2,3] },
